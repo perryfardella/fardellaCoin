bf1d45b7fbf2a409d0450aedce1641162379ceea
Perry Fardella <perry.fardella@live.com>
2021-03-18 08:29:28 +0800

    Create new block class

---
+++ block.py
@@ -0,0 +1,12 @@
+# import datetime library
+import datetime
+
+# print current date and time
+x = datetime.datetime.now()
+print(x)
+
+
+class Block:
+    # default constructor for block class
+    def __init__():
+        pass
