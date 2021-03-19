1bbacf9568f7cbd4b8eaa4799c502e3729f57293
Perry Fardella <perry.fardella@live.com>
2021-03-20 07:43:55 +0800

    Add print block and generate hash methods

--- block.py
+++ block.py
@@ -1,14 +1,20 @@
-# import datetime library
-import datetime
-
-# print current date and time
-print(datetime.datetime.now())
+from datetime import datetime
+from hashlib import sha256
 
 
 class Block:
-    # default constructor for block class
     def __init__(self, transactions, previous_hash, nonce=0):
         self.transactions = transactions
         self.previous_hash = previous_hash
         self.nonce = nonce
         self.timestamp = datetime.now()
+
+    def print_block(self):
+        print("timestamp:", self.timestamp)
+        print("transactions:", self.transactions)
+        print("current hash:", self.generate_hash())
+
+    def generate_hash(self):
+        block_contents = str(
+            self.timestamp) + str(self.transactions) + str(self.nonce) + str(self.previous_hash)
+        return block_contents
