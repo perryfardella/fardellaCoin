95c809fd9ad58366ef28f87325afa494679d04aa
Perry Fardella <perry.fardella@live.com>
2021-03-21 13:54:37 +0800

    Add chain validation method

--- blockchain.py
+++ blockchain.py
@@ -8,6 +8,7 @@ class Blockchain:
         self.all_transactions = []
         self.genesis_block()
 
+    # method to establish the genesis block
     def genesis_block(self):
         genesis_block = Block([], 0)
         self.chain.append(genesis_block)
@@ -19,8 +20,21 @@ class Blockchain:
             print("Block {} {}".format(i, current_block))
             current_block.print_contents()
 
-  # add block to blockchain `chain`
+    # add block to blockchain `chain`
     def add_block(self, transactions):
         previous_block_hash = self.chain[len(self.chain)-1].hash
         new_block = Block(transactions, previous_block_hash)
         self.chain.append(new_block)
+
+    def validate_chain(self):
+        for i in range(1, len(self.chain)):
+            current = self.chain[i]
+            previous = self.chain[i-1]
+
+            if current.hash != current.generate_hash():
+                return 0
+
+            if previous.hash != previous.generate_hash():
+                return 0
+
+        return 1
