57a475509b4571fab865fb3dae3f8a96c95c1949
Perry Fardella <perry.fardella@live.com>
2021-03-21 14:50:30 +0800

    Create a new scripting file to test the proof of work functionality

---
+++ script2.py
@@ -0,0 +1,19 @@
+from hashlib import sha256
+new_transactions = [{'amount': '30', 'sender': 'alice', 'receiver': 'bob'},
+                    {'amount': '55', 'sender': 'bob', 'receiver': 'alice'}]
+
+# import sha256
+
+# sets the amount of leading zeros that must be found in the hash produced by the nonce
+difficulty = 2
+nonce = 0
+
+# creating the proof
+
+# printing proof
+
+
+# finding a proof that has 2 leading zeros
+
+
+# printing final proof that was found
