b65be0d38daebc86ff81090e2e74f8dd4d0a6c98
Perry Fardella <perry.fardella@live.com>
2021-03-21 17:56:40 +0800

    Update script 2 with code to find a proof

--- script2.py
+++ script2.py
@@ -9,11 +9,16 @@ difficulty = 2
 nonce = 0
 
 # creating the proof
+proof = sha256((str(nonce) + str(new_transactions)).encode()).hexdigest()
 
 # printing proof
-
+print(proof)
 
 # finding a proof that has 2 leading zeros
-
+while (proof[:2] != '0' * difficulty):
+    nonce += 1
+    proof = sha256((str(nonce) + str(new_transactions)).encode()).hexdigest()
 
 # printing final proof that was found
+final_proof = proof
+print(final_proof)
