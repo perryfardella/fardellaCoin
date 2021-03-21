fca8662c81399e8e1b2040f3b4cae9a8735cc8e1
Perry Fardella <perry.fardella@live.com>
2021-03-21 10:49:38 +0800

    Add automatic creation of the genesis block to the blockchain constructor

--- blockchain.py
+++ blockchain.py
@@ -6,6 +6,7 @@ class Blockchain:
     def __init__(self):
         self.chain = []
         self.all_transactions = []
+        self.genesis_block()
 
     def genesis_block(self):
         genesis_block = Block([], 0)
